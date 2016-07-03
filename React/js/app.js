d95ba9cd996f1210e9f53a0d07ef0fa81e07e6a1
Ilya Timofeev <il.tmfv@gmail.com>
2016-07-03 15:48:40 +0300

    Added news component

--- React/js/app.js
+++ React/js/app.js
@@ -1,8 +1,19 @@
+var News = React.createClass({
+  render: function() {
+    return (
+      <div className="news">
+        К сожалению, новостей нет.
+      </div>
+    );
+  }
+});
+
 var App = React.createClass({
   render: function() {
     return (
       <div className="app">
-        Всем привет, я компонент App!
+        Всем привет, я компонент App! Я умею отображать новости.
+        <News />
       </div>
     );
   }
