ed2ceaa7f732cf5bc16d76b57cfee8c624edaf51
Ilya Timofeev <il.tmfv@gmail.com>
2016-07-03 22:58:38 +0300

    Added new field to News + link

--- React/js/app.js
+++ React/js/app.js
@@ -1,15 +1,18 @@
 var my_news = [
   {
     author: 'Саша Печкин',
-    text: 'В четверг, четвертого числа...'
+    text: 'В четчерг, четвертого числа...',
+    bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
   },
   {
     author: 'Просто Вася',
-    text: 'Считаю, что $ должен стоить 35 рублей!'
+    text: 'Считаю, что $ должен стоить 35 рублей!',
+    bigText: 'А евро 42!'
   },
   {
     author: 'Гость',
-    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000'
+    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
+    bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
   }
 ];
 
@@ -17,17 +20,21 @@ var Article = React.createClass({
   propTypes: {
     data: React.PropTypes.shape({
       author: React.PropTypes.string.isRequired,
-      text: React.PropTypes.string.isRequired
+      text: React.PropTypes.string.isRequired,
+      bigText: React.PropTypes.string.isRequired
     })
   },
   render: function() {
     var author = this.props.data.author,
-        text = this.props.data.text;
+        text = this.props.data.text,
+        bigText = this.props.data.bigText;
 
     return (
       <div className="article">
         <p className="news__author">{author}:</p>
         <p className="news__text">{text}</p>
+        <a href="#" className='news__readmore'>Подробнее</a>
+        <p className='news__big-text'>{bigText}</p>
       </div>
     )
   }
