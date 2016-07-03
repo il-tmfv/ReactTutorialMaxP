0c7c50708cd8cd909c01aae224534bff42d5c870
Ilya Timofeev <il.tmfv@gmail.com>
2016-07-03 22:38:20 +0300

    Added news counter

--- React/js/app.js
+++ React/js/app.js
@@ -16,18 +16,25 @@ var my_news = [
 var News = React.createClass({
   render: function() {
     var data = this.props.data;
-    var newsTemplate = data.map(function(item, index) {
-      return (
-        <div key={index}>
-          <p className="news__author">{item.author}:</p>
-          <p className="news__text">{item.text}</p>
-        </div>
-      )
-    })
+    var newsTemplate 
+    if (data.length > 0) {
+      newsTemplate = data.map(function(item, index) {
+        return (
+          <div key={index}>
+            <p className="news__author">{item.author}:</p>
+            <p className="news__text">{item.text}</p>
+          </div>
+        )
+      })
+    }
+    else {
+      newsTemplate = <p>К сожалению новостей нет</p>
+    }
 
     return (
       <div className="news">
         {newsTemplate}
+        <strong className={data.length > 0 ? '':'none'}>Всего новостей: {data.length}</strong>
       </div>
     );
   }
