c3235ed855bb7fc432991ca516b367b364ba4e6a
Ilya Timofeev <il.tmfv@gmail.com>
2016-07-03 15:53:10 +0300

    Added comments component

--- React/js/app.js
+++ React/js/app.js
@@ -8,12 +8,23 @@ var News = React.createClass({
   }
 });
 
+var Comments = React.createClass({
+  render: function() {
+    return (
+      <div className="comments">
+        Нет новостей - комментировать нечего
+      </div>
+    );
+  }
+});
+
 var App = React.createClass({
   render: function() {
     return (
       <div className="app">
         Всем привет, я компонент App! Я умею отображать новости.
         <News />
+        <Comments />
       </div>
     );
   }
