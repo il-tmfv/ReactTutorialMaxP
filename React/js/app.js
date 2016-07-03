cd26658744ef544400e914f8a635d218f326c602
Ilya Timofeev <il.tmfv@gmail.com>
2016-07-03 15:46:46 +0300

    Added app component

--- React/js/app.js
+++ React/js/app.js
@@ -1,4 +1,14 @@
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="app">
+        Всем привет, я компонент App!
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
 );
