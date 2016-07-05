c06b1efa70937dc39cdaf07f1d0487fc3e657d2c
Ilya Timofeev <il.tmfv@gmail.com>
2016-07-05 11:44:07 +0300

    Added ESlint

--- Redux/webpack.config.js
+++ Redux/webpack.config.js
@@ -2,13 +2,22 @@ var path = require('path')
 
 var webpack = require('webpack')
 
-module.exports = {
+module.exports = {    
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
     './src/index'
   ],
   module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loaders: ['eslint'],
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+      }
+    ],
     loaders: [
       {
         loaders: ['react-hot', 'babel-loader'],
@@ -27,7 +36,6 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.HotModuleReplacementPlugin()
   ]
 }
