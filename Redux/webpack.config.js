12f9bd0ba4ab9aada6376ac3134062ee8b2adacf
Ilya Timofeev <il.tmfv@gmail.com>
2016-07-06 12:57:16 +0300

    Added styles to app

--- Redux/webpack.config.js
+++ Redux/webpack.config.js
@@ -2,7 +2,10 @@ var path = require('path')
 
 var webpack = require('webpack')
 
-module.exports = {    
+var autoprefixer = require('autoprefixer')
+var precss = require('precss')
+
+module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
@@ -26,8 +29,15 @@ module.exports = {
         ],
         test: /\.js$/,
         plugins: ['transform-runtime']
+      },
+      {
+        test:   /\.css$/,
+        loader: "style-loader!css-loader!postcss-loader"
       }
     ]
+  },
+  postcss: function () {
+    return [autoprefixer, precss];
   },
   output: {
     path: path.join(__dirname, 'dist'),
